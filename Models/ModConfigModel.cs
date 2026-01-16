1947684c4ff8b9d81ba139ec56b3b354fa023645
Trinagan <piotrlici@gmail.com>
2026-01-16 14:16:42 +0000

    Inital commit
    Creating the repo and committing the release version of the mod

---
+++ Models/ModConfigModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeHelmetVisor.Models
+{
+    public record ModConfig
+    {
+        public bool AllFaceShields { get; set; }
+        public string MaskType { get; set; }
+        public Dictionary<string, string> SpecificFaceShields { get; set; } = new();
+    }
+}
