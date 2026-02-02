b8a27121a4a2b3c4eaa28a9654a9824f32467069
Trinagan <piotrlici@gmail.com>
2026-02-02 19:33:59 +0000

    Update ChangeHelmetVisorServer.cs

--- Server/ChangeHelmetVisorServer.cs
+++ Server/ChangeHelmetVisorServer.cs
@@ -17,7 +17,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Change Helmet Visor";
     public override string Author { get; init; } = "Trinagan";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.11");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
